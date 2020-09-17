ac8828455b4300238365fff40c7d533b876cd06f
amirali175 <amirdek511@gmail.com>
2020-09-17 14:03:18 -0400

    :horse:

---
+++ 2.Web/300116685/mon_fichier_javascript.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>mon fichier</title>
+  </head> Wabery_shop
+  <body>
+  <h1> page d'acceuil 
+  <p2> 
+  
+  </body>
+  <script src="/monScript.js"></script>
+</html>
