74193cae633e59dc8e815a4e884bb9fffbaad89a
b300098957 <300098957@collegeboreal.ca>
2020-11-21 19:55:58 -0500

    .

--- A.Animations/b300098957/src/app/home/home.component.ts
+++ A.Animations/b300098957/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import { Application } from "@nativescript/core";
-import { View } from "@nativescript/core";
 
 @Component({
     selector: "Home",
@@ -23,21 +22,4 @@ export class HomeComponent implements OnInit {
         sideDrawer.showDrawer();
     }
 
-    // animate(view: View) {
-    //     let duration = 300;
-    //     view.animate({ opacity: 0, duration: duration })
-    //         .then(() => view.animate({ opacity: 1, duration: duration }))
-    //         .then(() => view.animate({ translate: { x: 200, y: 200 }, duration: duration }))
-    //         .then(() => view.animate({ translate: { x: 0, y: 0 }, duration: duration }))
-    //         .then(() => view.animate({ scale: { x: 5, y: 5 }, duration: duration }))
-    //         .then(() => view.animate({ scale: { x: 1, y: 1 }, duration: duration }))
-    //         .then(() => view.animate({ rotate: 180, duration: duration }))
-    //         .then(() => view.animate({ rotate: 0, duration: duration }))
-    //         .then(() => {
-    //             console.log("Animation finished");
-    //         })
-    //         .catch((e) => {
-    //             console.log(e.message);
-    //         });
-    // }
 }
