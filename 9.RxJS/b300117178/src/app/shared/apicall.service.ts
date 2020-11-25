a3b72ff6699d725fd55e346d419180435c01728b
willfrid boris <boriskwayep35@yahoo.com>
2020-11-25 14:56:48 -0500

    :whale2:

---
+++ 9.RxJS/b300117178/src/app/shared/apicall.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import {catchError, map} from "rxjs/operators";
+import { Country } from './country';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApicallService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  searchCountryByName(name: string): Observable<Country[]>{
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Accept', 'application/json');
+    headers = headers.append(
+        'X-RapidAPI-Key',
+        '----------hf11c17c4fea2b3dp179054jsn24----------'
+    );
+    return this.httpClient.get(
+        `https://restcountries-v1.p.rapidapi.com/capital/` + name,
+        {headers: headers}
+    ).pipe(
+        map((data: Country[]) => {
+            return data;
+        }), catchError( error => {
+            return throwError( 'Capital not found!' );
+        })
+    )
+  }
+}
