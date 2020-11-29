985d8054484ddd16b28f67d417b1a69ebb261611
zoureni <zoureni@outlook.fr>
2020-11-28 20:21:27 -0500

    zoureni

---
+++ A.Animations/b300118524/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from "@angular/core";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { Application } from "@nativescript/core";
+import { FormBuilder  } from "@angular/forms";
+
+@Component({
+    selector: "Home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.scss"]
+})
+export class HomeComponent implements OnInit {
+
+    constructor() {
+        // Use the component constructor to inject providers.
+    }
+
+    ngOnInit(): void {
+        // Init your component properties here.
+        
+    }
+
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>Application.getRootView();
+        sideDrawer.showDrawer();
+    }
+}
