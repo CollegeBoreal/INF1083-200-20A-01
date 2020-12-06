156060da3ffd0ea808144e034387231c8f2e4891
Auriane KOUTA-DEASSIO <kdmarieauriane@gmail.com>
2020-12-05 22:04:40 -0500

    Hey c'est Auriane

--- A.Animations/b300116670/src/app/home/home.module.ts
+++ A.Animations/b300116670/src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "@nativescript/angular";
 
 import { HomeRoutingModule } from "./home-routing.module";
-import { HomeComponent } from "./home.component";
+import { HomeComponent } from "./HomeComponent";
 
 @NgModule({
     imports: [
