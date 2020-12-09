24a413a404b47347781476fca20efaedd7e2a6ef
dido05327 <idirabib@outlook.fr>
2020-12-09 00:11:51 -0500

    Idir

--- 9.RxJS/b300117444/src/app/app.module.ts
+++ 9.RxJS/b300117444/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "@nativescript/angular";
+import { NativeScriptHttpClientModule } from "@nativescript/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -10,7 +11,8 @@ import { AppComponent } from "./app.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpClientModule
     ],
     declarations: [
         AppComponent
--- 9.RxJS/b300117444/src/app/shared/apicall.service.ts
+++ 9.RxJS/b300117444/src/app/shared/apicall.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import {catchError, map} from "rxjs/operators";
+import { Country } from './country';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApicallService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  searchCountryByName(name: string): Observable<Country[]>{
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Accept', 'application/json');
+    headers = headers.append(
+        'X-RapidAPI-Key',
+        '----------fdcdde4103mshd01f12034bb78c2p14e0e3jsndeb7fa6cc3fc----------'
+    );
+    return this.httpClient.get(
+        `https://restcountries-v1.p.rapidapi.com/capital/` + name,
+        {headers: headers}
+    ).pipe(
+        map((data: Country[]) => {
+            return data;
+        }), catchError( error => {
+            return throwError( 'Capital not found!' );
+        })
+    )
+  }
 }
