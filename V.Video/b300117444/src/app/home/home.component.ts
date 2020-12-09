57c20a6cbad2db48280a403a3b86168183616a18
dido05327 <idirabib@outlook.fr>
2020-12-09 00:50:51 -0500

    Idirrr

--- V.Video/b300117444/src/app/home/home.component.ts
+++ V.Video/b300117444/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-
+import { registerElement } from "@nativescript/angular";
+import { Video } from '@nstudio/nativescript-exoplayer';
+registerElement("Video", () => Video);
 @Component({
     selector: "Home",
     templateUrl: "./home.component.html"
