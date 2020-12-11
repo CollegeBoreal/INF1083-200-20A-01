f314925885068da3b461d524d148efee05f11d7b
ZackTO <zackstoronto@gmail.com>
2020-12-11 14:07:25 -0500

    wellness

--- P.Projects/b300115140/Appwellness/wellness/src/app/shared/data.service.ts
+++ P.Projects/b300115140/Appwellness/wellness/src/app/shared/data.service.ts
@@ -16,25 +16,25 @@ export class DataService {
         {
             id: 1,
             name: "Face Mask and Creme de Jour",
-            description: "Description for Item 1",
+            description: "Choose our most popular face cream",
             avatar: "https://www.monvanityideal.com/data/produits/37/43/u/juvaflorine-masque-purifiant-argile-849e6aeb7e1d7a99dd9e11cbf1827f2a.jpg"
         },
         {
             id: 2,
             name: "Body Cream",
-            description: "Description for Item 2",
+            description: "This lavender body cream",
             avatar: "https://www.cocooninglove.com/wp-content/uploads/2017/11/A13_Exfoliant-fouette-Abricot-1.jpg"
         },
         {
             id: 7,
             name: "Relaxation & Massage",
-            description: "Description for Item 7",
+            description: "Take a reak for a full hour of swedish massage",
             avatar:"https://static1.squarespace.com/static/5e35fdfa8e5e5b2d211cd1bb/t/5ea6d86c192a052e37bdf3b3/1587992685574/1.jpg"
         },
         {
             id: 8,
             name: "Yoga Bikram",
-            description: "Description for Item 8",
+            description: "Yoga with a heated room and relaxation!",
             avatar:"https://www.verywellfit.com/thmb/Zy3X7gStpSbz8N-cQKrDA4HMBb0=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/bikramclass-56aa42425f9b58b7d0035c9d.jpg"
     
         
