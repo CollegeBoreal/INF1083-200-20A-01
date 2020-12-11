c9fbdd1f50de97b775b929058ef6a2a251520faf
willfrid boris <boriskwayep35@yahoo.com>
2020-12-10 22:35:57 -0500

    :whale:

--- P.Projects/b300117178/src/app/home/home.component.ts
+++ P.Projects/b300117178/src/app/home/home.component.ts
@@ -9,10 +9,24 @@ import { DataService, DataItem } from "../shared/data.service";
 })
 export class HomeComponent implements OnInit {
     items: Array<DataItem>;
+    state:string;
+    
 
     constructor(private _itemService: DataService) { }
 
     ngOnInit(): void {
+        
         this.items = this._itemService.getItems();
+        this.state="Pause";
+        
     }
+    onTap(): void {
+        if(this.state=="Pause"){
+            this.state="Play";
+    
+        }else{
+                this.state="Pause";
+        }
+
+}
 }
