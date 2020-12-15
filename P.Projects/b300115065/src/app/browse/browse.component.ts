b0caedc0cdc645f80dcfb66447459c393e7e686c
fadde68 <54910778+fadde68@users.noreply.github.com>
2020-12-15 13:05:40 -0500

    Update browse.component.ts

--- P.Projects/b300115065/src/app/browse/browse.component.ts
+++ P.Projects/b300115065/src/app/browse/browse.component.ts
@@ -1,20 +1,15 @@
 import { Component, OnInit } from "@angular/core";
-// somewhere at top of your component or bootstrap file
-import { registerElement } from "@nativescript/angular";
-import { Video } from '@nstudio/nativescript-exoplayer';
-registerElement("Video", () => Video);
 
 @Component({
-    selector: "Home",
-    templateUrl: "./home.component.html"
+    selector: "Browse",
+    templateUrl: "./browse.component.html"
 })
-export class HomeComponent implements OnInit {
-
+export class BrowseComponent implements OnInit {
     constructor() {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
-        // Init your component properties here.
+        // Use the "ngOnInit" handler to initialize data for the view.
     }
 }
