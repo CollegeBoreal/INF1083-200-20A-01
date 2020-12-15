390dbbd0c3340d95e842113149b6990129882245
fadde68 <54910778+fadde68@users.noreply.github.com>
2020-12-15 13:10:50 -0500

    🎶

--- P.Projects/b300115065/src/app/browse/browse.component.ts
+++ P.Projects/b300115065/src/app/browse/browse.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+// somewhere at top of your component or bootstrap file
+import { registerElement } from "@nativescript/angular";
+import { Video } from '@nstudio/nativescript-exoplayer';
+registerElement("Video", () => Video);
 
 @Component({
     selector: "Browse",
