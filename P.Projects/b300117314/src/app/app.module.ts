094ac906af1e5c85cf0cb58197d31917b21a576b
morti747 <300117314@monboreal.ca>
2020-12-15 19:23:58 -0500

    :star:

---
+++ P.Projects/b300117314/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "@nativescript/angular";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NativeScriptHttpClientModule } from "@nativescript/angular";
+
+
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptHttpClientModule,
+        NativeScriptModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
